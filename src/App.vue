dbfcc4cbffc4d7f279eedb7e28d11f7c2c31b4a3
Matt <matt@192.168.0.106>
2019-11-08 00:48:11 +0800

    router worked

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
     <Heador />
     <router-view></router-view>
     <Footer/>
--- src/js/home/hslider.vue
+++ src/js/home/hslider.vue
@@ -21,39 +21,39 @@ export default {
   data() {
     return {
       items:[{
-        href: "App",
+        href: "home",
         src: "//m.360buyimg.com/mobilecms/s700x280_jfs/t1/103835/16/1563/88525/5dc23395E2d513e20/c49dfbf88de32903.jpg!cr_1125x445_0_171!q70.jpg.dpg"
       },
       {
-        href: "App",
+        href: "home",
         src: "//m.360buyimg.com/mobilecms/s700x280_jfs/t1/24046/10/6380/94217/5c511b9dEcc9ba39c/7913173ae5ed0617.jpg!cr_1125x445_0_171!q70.jpg.dpg"
       },
       {
-        href: "App",
+        href: "home",
         src: "//m.360buyimg.com/mobilecms/s700x280_jfs/t1/45055/35/15168/115704/5dbf9bd3E53bcbcfb/a49a8beacd926951.jpg!cr_1125x445_0_171!q70.jpg.dpg"
       }],
       enters: [{
-        href: "App",
+        href: "home",
         img: "https://img12.360buyimg.com/jrpmobile/jfs/t3991/64/2521945388/12110/93c0139/58d1e462Ncf294123.png?width=132&height=132",
         title: "优惠券"
       },
       {
-        href: "App",
+        href: "home",
         img: "https://img12.360buyimg.com/jrpmobile/jfs/t5590/252/875247023/17343/946aa72c/59224650N0f7ffc92.png?width=132&height=132",
         title: "抢福利"
       },
       {
-        href: "App",
+        href: "home",
         img: "https://img12.360buyimg.com/jrpmobile/jfs/t4393/329/2180608902/13217/c88c0cec/58ec9dcdN1534e2d7.png?width=132&height=132",
         title: "抢钢镚"
       },
       {
-        href: "App",
+        href: "home",
         img: "https://img12.360buyimg.com/jrpmobile/jfs/t5488/298/1036263348/12073/b4f4de97/590ac8e8Ne9def22e.png?width=135&height=135",
         title: "白条提额"
       },
       {
-        href: "App",
+        href: "home",
         img: "https://img12.360buyimg.com/jrpmobile/jfs/t3991/64/2521945388/12110/93c0139/58d1e462Ncf294123.png?width=132&height=132",
         title: "优惠券"
       },],
